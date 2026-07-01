8910ab8f28dcdd7b3abf5f17eb337aa264f5c3af
Nguyen Tu <nguyenanhtukhtn@gmail.com>
2026-07-01 15:11:07 +0700

    Add supabase key for staging server

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
+  supabaseUrl: 'https://prylmjsrkdowekgfxysw.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByeWxtanNya2Rvd2VrZ2Z4eXN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4NTg3MjksImV4cCI6MjA5ODQzNDcyOX0.uldRQZTjeKeLXYd5bK0KiWFyFrhyROzD0PTE8C3XWTM',
+  supabaseBucket: 'tour-management',
   useMockData: false
 };
